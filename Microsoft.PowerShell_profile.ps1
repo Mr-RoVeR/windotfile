6979f9a284cda8a825e61712ae5db25f4b85589b
Mr-RoVeR <harshadeshik1@gmail.com>
2024-12-09 01:48:13 +0530

    fastfetch config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
+j
+$env:STARSHIP_CONFIG = "C:\Users\harsh\.config\starship.toml"
 Invoke-Expression (&starship init powershell)
-
-starship preset pastel-powerline -o $HOME\.config\starship.toml
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
@@ -11,9 +11,37 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
         }
 }
+Import-Module -Name Terminal-Icons
+Import-Module -Name Winget-Essentials
+Import-Module -Name ModuleFast
+Import-Module -Name ChocolateyGet
+
 
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-Alias yt-vid 'yt-dlp'
+function yt-vid {
+    yt-dlp -f "bv*+ba/b" -S "res,ext:webm:m4a" --merge-output-format mkv -o "%(title)s.%(ext)s" $args
+}
+Set-Alias yt-ply 'yt-dlp'
+function yt-ply {
+    yt-dlp -f "bv*+ba/b" -S "res,ext:webm:m4a" --merge-output-format mkv -o "%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s" $args
+}
+
+function conv-help {
+    Write-Host "ffmpeg -i input.mkv -i audio.m4a -map 0 -map 1 -c copy output.mkv"
+}
+#Nav Alias
+Set-Alias .. 'back1'
+Set-Alias ... 'back2'
+Set-Alias .... 'back3'
+Set-Alias ..... 'back4'
+
+function back1 { Set-Location .. }
+function back2 { Set-Location ../.. }
+function back3 { Set-Location ../../.. }
+function back4 { Set-Location ../../../.. }
